13da3389e4c568dcad8cd45264d708d86aa3accb
Mahmoud Elhendy <mahmoud.ali.elhendy1990@gmail.com>
2025-06-21 12:02:54 +0300

    scan unterm str

--- app/main.py
+++ app/main.py
@@ -1,21 +1,32 @@
 import sys
 
+from enum import Enum, auto
+
+
+class Err(Enum):
+    NONE = auto()
+    UNEXPECTED_CHAR = auto()
+    UNTERMINATED_STRING = auto()
+
 
 class Token:
-    def __init__(self, type: str, lexme: str, literal: str, line: int, err: bool = False) -> None:
+    def __init__(self, type: str, lexme: str, literal: str, line: int, err: Err = Err.NONE) -> None:
         self.type: str = type
         self.lexme: str = lexme
         self.literal: str = literal
         self.line: int = line
-        self.err: bool = err
+        self.err: Err = err
 
     def __str__(self) -> str:
         return f'{self.type} {self.lexme} {self.literal}'
 
-    def display(self):
-        if self.err:
+    def display(self) -> None:
+        if self.err == Err.UNEXPECTED_CHAR:
             print(
                 f'[line {self.line}] Error: Unexpected character: {self.lexme}', file=sys.stderr)
+        elif self.err == Err.UNTERMINATED_STRING:
+            print(
+                f'[line {self.line}] Error: Unterminated string.', file=sys.stderr)
         else:
             print(self)
 
@@ -52,6 +63,9 @@ class Scanner:
             self.line += 1
             char = 0
             while char < len(line):
+                type: str = ''
+                literal: str = ''
+                err: Err = Err.NONE
                 token_str: str = line[char]
                 if token_str.isspace():
                     char += 1
@@ -69,26 +83,46 @@ class Scanner:
                     if line[char + 1] == '=':
                         char += 1
                         token_str += line[char]
+                # handle comments
                 elif token_str == '/' and char + 1 < len(line) and line[char + 1] == '/':
                     # // comment -> skip the rest of line
                     break
+                elif token_str == '"':
+                    if (ret := self.scan_string_literals(line[char+1:])):
+                        type, literal = ret
+                        token_str = f'"{literal}"'
+                        char += len(literal) + 1
+                    else:
+                        self.ret = 65
+                        err = Err.UNTERMINATED_STRING
+                elif token_str not in self.lexmes:
+                    self.ret = 65
+                    err = Err.UNEXPECTED_CHAR
 
-                self.add_token(self.tokenize(token_str, self.line))
+                self.add_token(self.tokenize(
+                    token_str, self.line, type=type, literal=literal, err=err))
                 char += 1
         self.add_token(Token('EOF', '', 'null', self.line + 1))
 
-    def tokenize(self, lexme: str, line: int) -> Token:
-        if lexme in self.lexmes:
-            return Token(self.lexmes[lexme], lexme, 'null', line)
+    def scan_string_literals(self, line: str) -> tuple[str, str] | None:
+        end: int = line.find('"')
+        if end != -1:
+            return ('STRING', line[:end])
         else:
-            self.ret = 65
-            return Token('', lexme, '', line, err=True)
+            return None
+
+    def tokenize(self, lexme: str, line: int, type: str = '', literal: str = '', err: Err = Err.NONE) -> Token:
+        literal = literal if literal else 'null'
+        if not type and lexme in self.lexmes:
+            type = self.lexmes[lexme]
+        return Token(type, lexme, literal, line, err=err)
 
     def add_token(self, token: Token) -> None:
         self.tokens.append(token)
 
 
 def main():
+
     if len(sys.argv) < 3:
         print("Usage: ./your_program.sh tokenize <filename>", file=sys.stderr)
         exit(1)
