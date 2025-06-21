0b9875a5effb8d74d224e84ee9323d9a0671c38c
Mahmoud Elhendy <mahmoud.ali.elhendy1990@gmail.com>
2025-06-21 08:03:20 +0300

    scan errors

--- app/main.py
+++ app/main.py
@@ -2,13 +2,22 @@ import sys
 
 
 class Token:
-    def __init__(self, type: str, lexme: str, literal: str) -> None:
-        self.type = type
-        self.lexme = lexme
-        self.literal = literal
+    def __init__(self, type: str, lexme: str, literal: str, line: int, err: bool = False) -> None:
+        self.type: str = type
+        self.lexme: str = lexme
+        self.literal: str = literal
+        self.line: int = line
+        self.err: bool = err
 
     def __str__(self) -> str:
         return f'{self.type} {self.lexme} {self.literal}'
+
+    def display(self):
+        if self.err:
+            print(
+                f'[Line {self.line}] Error: Unexpected character: {self.lexme}', file=sys.stderr)
+        else:
+            print(self)
 
 
 class Scanner:
@@ -18,17 +27,21 @@ class Scanner:
         self.line: int = 0
         self.lexmes: dict[str, str] = {
             '(': 'LEFT_PAREN', ')': 'RIGHT_PAREN', '{': 'LEFT_BRACE', '}': 'RIGHT_BRACE', '*': 'STAR', '.': 'DOT', ',': 'COMMA', '+': 'PLUS', '-': 'MINUS', ';': 'SEMICOLON', '/': 'SLASH'}
+        self.ret: int = 0
 
     def scan(self) -> None:
         for line in self.content:
             self.line += 1
             for lexme in line:
-                self.add_token(self.tokenize(lexme))
-        self.add_token(Token('EOF', '', 'null'))
+                self.add_token(self.tokenize(lexme, self.line))
+        self.add_token(Token('EOF', '', 'null', self.line + 1))
 
-    def tokenize(self, lexme: str) -> Token:
-        lexme_type: str = self.lexmes[lexme] if lexme in self.lexmes else ''
-        return Token(lexme_type, lexme, 'null')
+    def tokenize(self, lexme: str, line: int) -> Token:
+        if lexme in self.lexmes:
+            return Token(self.lexmes[lexme], lexme, 'null', line)
+        else:
+            self.ret = 65
+            return Token('', lexme, '', line, err=True)
 
     def add_token(self, token: Token) -> None:
         self.tokens.append(token)
@@ -53,7 +66,8 @@ def main():
         s = Scanner(file_contents)
         s.scan()
         for t in s.tokens:
-            print(t)
+            t.display()
+        sys.exit(s.ret)
     else:
         # Placeholder, replace this line when implementing the scanner
         print("EOF  null")
